5bd10fcdb325a791c94c8b0e762c01846720381d
Mauricio Girardi <mauricio.girardi@itsector.pt>
2026-06-03 21:36:56 +0100

    feat: create component button

---
+++ src/components/button.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/utils/cn'
+
+interface ButtonProps extends React.ComponentProps<'button'> {
+  className?: string
+}
+
+export function Button({ className, ...props }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className={cn(
+        'w-full mt-5 flex font-semibold items-center gap-2 cursor-pointer justify-center text-white bg-blue-400 rounded-sm h-12 hover:bg-blue-500',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
