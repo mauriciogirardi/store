00cdfcefcce519c1a4a9b98bfbf62fe8b82c2072
Mauricio Girardi <mauricio.girardi@itsector.pt>
2026-06-03 21:39:02 +0100

    chore: install some lib and config biome

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-};
+  images: {
+    remotePatterns: [new URL('https://fakestoreapi.com/img/**')],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  );
-}
